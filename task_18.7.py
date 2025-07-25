80ea98bb8e27a496498aa6e9374c5014c836a285
ayup-mar <maryam.ayupova@outlook.com>
2025-07-25 16:25:01 +0200

    task_18.7.py

--- task_18.7.py
+++ task_18.7.py
@@ -6,7 +6,7 @@ class_ = input('Введите предмет: ')
 # если данные введены верно
 if student in students_marks.keys() and class_ in students_marks[student].keys():
                 # удаляем оценку для ученика по предмету
-                students_marks[student][class_].append(mark)
+                students_marks[student][class_].remove(mark)
                 print(f'Для {student} по предмету {class_} удалена оценка {mark}')
 # неверно введены название предмета или имя ученика
 else:
